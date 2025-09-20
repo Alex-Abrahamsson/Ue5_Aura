bc1aa81f6f26ec71e5219c5d2c0fc826b519b6fa
Alexander Abrahamsson <79103208+Alex-Abrahamsson@users.noreply.github.com>
2025-09-21 00:29:20 +0200

    Nr130

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright AbraCode
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInormation.FindChecked(CharacterClass);
+}
